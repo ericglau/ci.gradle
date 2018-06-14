8bd03460b6506ec87d6494ff347828707765f225
Eric Lau <ericglau@ca.ibm.com>
2018-06-14 11:36:34 -0400

    Improve code readability

--- src/main/groovy/net/wasdev/wlp/gradle/plugins/tasks/InstallFeatureTask.groovy
+++ src/main/groovy/net/wasdev/wlp/gradle/plugins/tasks/InstallFeatureTask.groovy
@@ -15,6 +15,8 @@
  */
 package net.wasdev.wlp.gradle.plugins.tasks
 
+import java.util.Set
+
 import org.gradle.api.artifacts.Dependency
 import org.gradle.api.logging.LogLevel
 import org.gradle.api.tasks.TaskAction
@@ -35,106 +37,106 @@ class InstallFeatureTask extends AbstractServerTask {
 
     private class InstallFeatureTaskUtil extends InstallFeatureUtil {
         public InstallFeatureTaskUtil(File installDir, String from, String to, Set<String> pluginListedEsas)  throws PluginScenarioException, PluginExecutionException {
-            super(installDir, from, to, pluginListedEsas);
+            super(installDir, from, to, pluginListedEsas)
         }
 
         @Override
         public void debug(String msg) {
-            logger.debug(msg);
+            logger.debug(msg)
         }
 
         @Override
         public void debug(String msg, Throwable e) {
-            logger.debug(msg, e);
+            logger.debug(msg, e)
         }
 
         @Override
         public void debug(Throwable e) {
-            logger.debug(e);
+            logger.debug(e)
         }
 
         @Override
         public void warn(String msg) {
-            logger.warn(msg);
+            logger.warn(msg)
         }
 
         @Override
         public void info(String msg) {
-            logger.info(msg);
+            logger.info(msg)
         }
 
         @Override
         public boolean isDebugEnabled() {
-            return logger.isEnabled(LogLevel.DEBUG);
+            return logger.isEnabled(LogLevel.DEBUG)
         }
 
         @Override
         public File downloadArtifact(String groupId, String artifactId, String type, String version) throws PluginExecutionException {
-            String coordinates = groupId + ":" + artifactId + ":" + version + "@" + type;
-            def dep = project.dependencies.create(coordinates);
+            String coordinates = groupId + ":" + artifactId + ":" + version + "@" + type
+            def dep = project.dependencies.create(coordinates)
             def config = project.configurations.detachedConfiguration(dep)
 
             Set<File> files = new HashSet<File>()
             config.resolvedConfiguration.resolvedArtifacts.each { artifact ->
-                def artifactFile = artifact.file
+                File artifactFile = artifact.file
                 files.add(artifactFile)
                 debug(artifactFile.toString())
             }
 
-            if (files.size() == 0) {
-                throw new PluginExecutionException("Could not find artifact with coordinates " + coordinates);
+            if (!files) {
+                throw new PluginExecutionException("Could not find artifact with coordinates " + coordinates)
             }
-            return files.iterator().next();
+            return files.iterator().next()
         }
     }
 
     @TaskAction
     void installFeature() {
-        def pluginListedFeatures = getPluginListedFeatures(false);
-        def pluginListedEsas = getPluginListedFeatures(true);
+        def pluginListedFeatures = getPluginListedFeatures(false)
+        def pluginListedEsas = getPluginListedFeatures(true)
 
-        InstallFeatureUtil util;
+        InstallFeatureUtil util
         try {
-            util = new InstallFeatureTaskUtil(getInstallDir(project), server.features.from, server.features.to, pluginListedEsas);
+            util = new InstallFeatureTaskUtil(getInstallDir(project), server.features.from, server.features.to, pluginListedEsas)
         } catch (PluginScenarioException e) {
-            logger.debug(e.getMessage());
-            logger.debug("Installing features from installUtility.");
+            logger.debug(e.getMessage())
+            logger.debug("Installing features from installUtility.")
             installFeatureFromAnt()
-            return;
+            return
         }
 
-        def dependencyFeatures = getDependencyFeatures();
-        def serverFeatures = util.getServerFeatures(getServerDir(project), pluginListedFeatures.isEmpty());
+        def dependencyFeatures = getDependencyFeatures()
+        def serverFeatures = util.getServerFeatures(getServerDir(project), pluginListedFeatures.isEmpty())
 
-        Set<String> featuresToInstall = InstallFeatureUtil.combineToSet(pluginListedFeatures, dependencyFeatures, serverFeatures);
+        Set<String> featuresToInstall = InstallFeatureUtil.combineToSet(pluginListedFeatures, dependencyFeatures, serverFeatures)
 
-        util.installFeatures(server.features.acceptLicense, new ArrayList<String>(featuresToInstall));
+        util.installFeatures(server.features.acceptLicense, new ArrayList<String>(featuresToInstall))
     }
 
-    private def getPluginListedFeatures(boolean findEsaFiles) {
-        def features = server.features.name;
-        def result = new HashSet<String>();
+    private Set<String> getPluginListedFeatures(boolean findEsaFiles) {
+        def features = server.features.name
+        Set<String> result = new HashSet<String>()
         for (String feature : features) {
             if ((findEsaFiles && feature.endsWith(".esa"))
-            || !findEsaFiles && !feature.endsWith(".esa")) {
-                result.add(feature);
-                logger.debug(("Plugin listed " + (findEsaFiles ? "ESA" : "feature") + ": " + feature));
+                    || (!findEsaFiles && !feature.endsWith(".esa"))) {
+                result.add(feature)
+                logger.debug(("Plugin listed " + (findEsaFiles ? "ESA" : "feature") + ": " + feature))
             }
         }
-        return result;
+        return result
     }
 
-    private def getDependencyFeatures() {
-        def features = new HashSet<String>();
+    private Set<String> getDependencyFeatures() {
+        Set<String> features = new HashSet<String>()
         project.configurations.libertyFeature.dependencies.each { dep ->
             logger.debug("Dependency feature: " + dep.name)
             features.add(dep.name)
         }
-        return features;
+        return features
     }
 
     private def buildAntParams() {
-        def params = buildLibertyMap(project);
+        def params = buildLibertyMap(project)
         params.put('acceptLicense', server.features.acceptLicense)
         if (server.features.name != null) {
             params.put('name', server.features.name.join(","))
